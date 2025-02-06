fdf6d27a2ad56d487315805a9dd265cb044df759
TofaelKabir <tofaelkabir@gmail.com>
2025-02-05 21:39:27 -0500

    Python Basic 01

---
+++ Lec 01 Python Basic/01 Python Comment.py
@@ -0,0 +1,29 @@
+# Python Comment
+# This is an example of single line comment, interpreter ignore the Python comment
+# Shortcut to create: control/command + /  or Shift + 3
+# Another type is multi line comments, how to do that?
+
+"""
+When we use 3 double quotation or 3 single quotation, it creates multiple line comments
+@Author Mohammad Sharkar
+Version: 1.1
+Date: 02/05/2025
+"""
+
+'''
+Here we used, 3 single quotation, but we will use double quotation instead of single quotation al through course
+'''
+
+
+# print() is called a function
+# more specifically library function, because it came from python library
+print("------------------------------------------")
+print("Hello World!")
+print('Good Evening!')
+# single quotation is ok too
+# we will use double quotation in Python to stay in the same page
+
+print("Tofael")
+print(125)
+print(3.44)
+
---
+++ Lec 01 Python Basic/02 Python Variables.py
@@ -0,0 +1,33 @@
+print("------------------------------------------")
+# here 'name' is a variable, which is holding a data
+name = "Mohammad Sharkar"
+print(name)
+
+age = 125
+print(age)
+
+grade = 3.44
+print(grade)
+
+usCitizen = True
+print(usCitizen)
+
+# Above 4 data is not same
+# str represent string/Text type, represent by double quotation
+print(type(name))
+
+# int represent integer/numeric type, represent by no quotation
+print(type(age))
+
+# float represent float/numeric type [number with decimal], represent by no quotation
+print(type(grade)) 
+
+# bool represent boolean type [True/False], represent by no quotation
+print(type(usCitizen))  
+
+'''
+1) Create a file name 'info.py'
+2) use variable to define your name, address, age, grade, us Citizen or not
+3) Print those variable and also their type
+
+'''
